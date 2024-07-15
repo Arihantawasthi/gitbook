c25ac032fea9ddfa0a8b96987eb2182d58f2a808
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-07-16 00:44:31 +0530

    feat: Created a simple logger

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,27 @@ import (
 	"gitbook/app"
 	"gitbook/app/storage"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 
 func muxWrap(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        slog.Info("Incoming Request", "X-Request-ID", r.Header.Get("X-Request-ID"), "method", r.Method, "path", r.URL.Path, "body", r.Body)
         next.ServeHTTP(w, r)
         w.Header().Add("Content-Type", "application/json")
     })
 }
 
-func main() {
+func init() {
+    logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+    slog.SetDefault(logger)
     storage.ConnectPGStorage()
+}
+
+func main() {
     router := http.NewServeMux()
     app.RegisterRoutes(router)
 
