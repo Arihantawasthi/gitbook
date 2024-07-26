18752d276480e548b072f7859fa12c0184df3b58
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-07-27 02:42:37 +0530

    feat: Moved repo response type into types

--- app/handler/handlers.go
+++ app/handler/handlers.go
@@ -2,49 +2,42 @@ package handler
 
 import (
 	"encoding/json"
+	"gitbook/app/types"
 	"gitbook/utils"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
 type RepoHandler struct {
-    repoPath string
+	repoPath string
 }
 
 func NewRepoHandler() *RepoHandler {
-    return &RepoHandler{
-        repoPath: os.Getenv("REPO_DIR"),
-    }
-}
-
-type RepoResponse struct {
-    RepoName string
-    Desc string
-    Author string
-    CreatedAt time.Time
-    LastCommit time.Time
+	return &RepoHandler{
+		repoPath: os.Getenv("REPO_DIR"),
+	}
 }
 
 func (h *RepoHandler) GetAllRepos(w http.ResponseWriter, r *http.Request) error {
-    var response []RepoResponse
-    cmd := exec.Command("ls")
-    cmd.Dir = h.repoPath
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        return utils.RaiseHTTPError(err, "cannot read the directory", 500)
-    }
+	var response []types.RepoResponse
+	cmd := exec.Command("ls")
+	cmd.Dir = h.repoPath
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return utils.RaiseHTTPError(err, "cannot read the directory", 500)
+	}
 
-    outputStr := string(output)
-    lines := strings.Split(outputStr, "\n")
+	outputStr := string(output)
+	lines := strings.Split(outputStr, "\n")
 
-    for _, name := range lines {
-        name = strings.TrimSuffix(name, ".git")
-        response = append(response, RepoResponse{RepoName: name})
-    }
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(response)
-    return nil
+	for _, name := range lines {
+		name = strings.TrimSuffix(name, ".git")
+        response = append(response, types.RepoResponse{Name: name})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+	return nil
 }
