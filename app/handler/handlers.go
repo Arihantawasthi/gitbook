e714056a733c9be1cc9be190251ee0c153392137
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-07-28 03:01:20 +0530

    feat: completed the get all repos api

--- app/handler/handlers.go
+++ app/handler/handlers.go
@@ -30,12 +30,19 @@ func (h *RepoHandler) GetAllRepos(w http.ResponseWriter, r *http.Request) error 
 	lines := strings.Split(output, "\n")
 
 	for _, name := range lines {
-        desc, err := RunCommand("cat", fmt.Sprintf("%s/%s/description", h.repoPath, name))
-        if err != nil {
-            return utils.RaiseHTTPError(err, "cannot read description", 500)
-        }
-        name = strings.TrimSuffix(name, ".git")
-        response = append(response, types.RepoResponse{Name: name, Desc: string(desc)})
+		desc, err := RunCommand("cat", fmt.Sprintf("%s/%s/description", h.repoPath, name))
+		if err != nil {
+			return utils.RaiseHTTPError(err, "cannot read description", 500)
+		}
+		repoDetails, err := getRepoDetails(h.repoPath, name)
+		if err != nil {
+			return utils.RaiseHTTPError(err, "cannot get commit history", 500)
+		}
+		createdAt := repoDetails["firstCommit"][0]
+		author := repoDetails["firstCommit"][1]
+		lastCommit := repoDetails["lastCommit"][0]
+		name = strings.TrimSuffix(name, ".git")
+		response = append(response, types.RepoResponse{Name: name, Desc: string(desc), CreatedAt: createdAt, Author: author, LastCommitAt: lastCommit})
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -43,13 +50,27 @@ func (h *RepoHandler) GetAllRepos(w http.ResponseWriter, r *http.Request) error 
 	return nil
 }
 
+func getRepoDetails(repoPath, name string) (map[string][]string, error) {
+	repoDetails := make(map[string][]string)
+	gitDir := fmt.Sprintf("--git-dir=%s/%s", repoPath, name)
+	output, err := RunCommand("git", gitDir, "log", "--format=%ad|%an", "--date=format:%b %d, %Y %I:%M %p", "--reverse")
+	fmt.Println(output)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return nil, err
+	}
+	commits := strings.Split(output, "\n")
+	repoDetails["firstCommit"] = strings.Split(commits[0], "|")
+	repoDetails["lastCommit"] = strings.Split(commits[len(commits)-1], "|")
+	return repoDetails, nil
+}
 
-func RunCommand(cmdName string , args ...string) (string, error) {
-    cmd := exec.Command(cmdName, args...)
-    output, err := cmd.Output()
-    if err != nil {
-        return "", err
-    }
-    outputStr := strings.TrimSuffix(string(output), "\n")
-    return outputStr, nil
+func RunCommand(cmdName string, args ...string) (string, error) {
+	cmd := exec.Command(cmdName, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	outputStr := strings.TrimSuffix(string(output), "\n")
+	return outputStr, nil
 }
